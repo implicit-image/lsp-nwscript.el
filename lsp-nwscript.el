d5e9a06e93317087240fb67fb709106cbce6b31e
Błażej Niewiadomski <blaz.nie@protonmail.com>
2024-06-27 20:48:12 +0200

    Fix punctuation

--- lsp-nwscript.el
+++ lsp-nwscript.el
@@ -1,44 +1,49 @@
+;;; package --- LSP support for nwscript
+
+;;; Commentary:
+
+;;; Code:
 
 (require 'lsp-mode)
 
 (defgroup lsp-nwscript nil
-  "LSP support for NWScript"
+  "LSP support for NWScript."
   :group 'lsp-mode
   :link '(url-link "https://github.com/implicit-image/lsp-nwscript.el")
   :package-version '(lsp-mode . "8.0.1"))
 
 (lsp-defcustom lsp-nwscript-server-type "node"
-  "What server to use"
+  "What server to use."
   :type '(choice
           (const "node" :doc "Use nwscript-ee-language-server"))
   :group 'lsp-nwscript
   :package-version '(lsp-mode . "8.0.1"))
 
 (defgroup lsp-nwscript-node nil
-  "LSP support for NWScript, using nwscript-ee-language-server"
+  "LSP support for NWScript, using nwscript-ee-language-server."
   :group 'lsp-mode
   :link '(url-link "https://github.com/PhilippeChab/nwscript-ee-language-server")
   :package-version '(lsp-mode . "8.0.1"))
 
 (defgroup lsp-nwscript-py nil
-  "LSP support for NWScript, using nwscript-lsp"
+  "LSP support for NWScript, using nwscript-lsp."
   :group 'lsp-mode
   :link '(url-link "https://github.com/jd28/nwscript-lsp")
   :package-version '(lsp-mode . "8.0.1"))
 
 (defcustom lsp-nwscript-node-server-path nil
-  "Path to server.js"
+  "Path to server.js."
   :type 'string
   :group 'lsp-nwscript-node)
 
 (defcustom lsp-nwscript-node-server-args '("--stdio")
-  "Arguments to pass to the server"
+  "Arguments to pass to the server."
   :type '(repeat string)
   :risky t
   :group 'lsp-nwscript-node)
 
 (lsp-defcustom lsp-nwscript-node-hover-function-comments nil
-  "Whether to display function comments (using function comments as docs)"
+  "Whether to display function comments (using function comments as docs)."
   :type 'boolean
   :group 'lsp-nwscript-node
   :package-version '(lsp-mode . "8.0.1")
@@ -46,42 +51,42 @@
 
 
 (lsp-defcustom lsp-nwscript-node-complete-function-params nil
-  "Whether to autocpmlete function parameters"
+  "Whether to autocpmlete function parameters."
   :type 'boolean
   :group 'lsp-nwscript-node
   :package-version '(lsp-mode . "8.0.1")
   :lsp-path "nwscript-ee-lsp.completion.addParamsToFunctions")
 
 (lsp-defcustom lsp-nwscript-node-format-enabled nil
-  "Whether to enable formatting"
+  "Whether to enable formatting."
   :type 'boolean
   :group 'lsp-nwscript-node
   :package-version '(lsp-mode . "8.0.1")
   :lsp-path "nwscript-ee-lsp.formatter.enabled")
 
 (lsp-defcustom lsp-nwscript-node-format-verbose nil
-  "Whether or not the formatter is verbose "
+  "Whether or not the formatter is verbose."
   :type 'boolean
   :group 'lsp-nwscript-node
   :package-version '(lsp-mode . "8.0.1")
   :lsp-path "nwscript-ee-lsp.formatter.verbose")
 
 (lsp-defcustom lsp-nwscript-node-format-executable "clang-format"
-  "Clang format executable path"
+  "Clang format executable path."
   :type 'string
   :group 'lsp-nwscript-node
   :package-version '(lsp-mode . "8.0.1")
   :lsp-path "nwscript-ee-lsp.formatter.executable")
 
 (lsp-defcustom lsp-nwscript-node-format-ignore-globs ()
-  "Glob patterns to ignore"
+  "Glob patterns to ignore."
   :type '(repeat string)
   :group 'lsp-nwscript-node
   :package-version '(lsp-mode . "8.0.1")
   :lsp-path "nwscript-ee-lsp.formatter.ignoreGlobs")
 
 (lsp-defcustom lsp-nwscript-node-compiler-enabled nil
-  "Whether or not the compiler is enabled"
+  "Whether or not the compiler is enabled."
   :type 'boolean
   :group 'lsp-nwscript-node
   :package-version '(lsp-mode . "8.0.1")
@@ -89,7 +94,7 @@
 
 
 (lsp-defcustom lsp-nwscript-node-compiler-os nil
-  "What compiler to use "
+  "What compiler to use."
   :type '(choice
           (const "Darwin")
           (const "Linux")
@@ -100,7 +105,7 @@
 
 
 (lsp-defcustom lsp-nwscript-node-compiler-verbose nil
-  "Whether or not the compiler is verbose"
+  "Whether or not the compiler is verbose."
   :type 'boolean
   :group 'lsp-nwscript-node
   :package-version '(lsp-mode . "8.0.1")
@@ -108,21 +113,21 @@
 
 
 (lsp-defcustom lsp-nwscript-node-compiler-report-warnings nil
-  "Whether or not the compiler should report warnings"
+  "Whether or not the compiler should report warnings."
   :type 'boolean
   :group 'lsp-nwscript-node
   :package-version '(lsp-mode . "8.0.1")
   :lsp-path "nwscript-ee-lsp.compiler.reportWarnings")
 
 (lsp-defcustom lsp-nwscript-node-compiler-nwn-home nil
-  "NWN home directory"
+  "NWN home directory."
   :type 'string
   :group 'lsp-nwscript-node
   :package-version '(lsp-mode . "8.0.1")
   :lsp-path "nwscript-ee-lsp.compiler.nwnHome")
 
 (lsp-defcustom lsp-nwscript-node-compiler-nwn-installation nil
-  "NWN installation directory"
+  "NWN installation directory."
   :type 'string
   :group 'lsp-nwscript-node
   :package-version '(lsp-mode . "8.0.1")
@@ -130,7 +135,7 @@
 
 (defcustom lsp-nwscript-node-extra-server-options ()
   "Use this to pass extra server configuration options. Same syntax as \
-`lsp-register-custom-settings'"
+`lsp-register-custom-settings'."
   :type '(alist :value-type (group integer boolean)
   :group 'lsp-nwscript-node
   :package-version '(lsp-mode . "8.0.1"))
@@ -138,7 +143,7 @@
 
 (defun lsp-nwscript-node--server-command ()
   "Generate LSP startup command for nwscript-ee-language-server."
-   '("node" lsp-nwscriptnode-server-path lsp-nwscript-node-server-args))
+   '("node" lsp-nwscript-node-server-path lsp-nwscript-node-server-args))
 
 
 ;; register extra server options
@@ -153,3 +158,4 @@
 (lsp-consistency-check lsp-nwscript)
 
 (provide 'lsp-nwscript)
+;;; lsp-nwscript.el ends here.
